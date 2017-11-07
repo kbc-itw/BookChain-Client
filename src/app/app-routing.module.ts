9eec891725965b00517362ee5c1a696ef61e226d
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-11-07 13:15:40 +0900

    ルートモジュールのルータはビューのルータに回すように

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './view/view.module#ViewModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
