f5cc8d5f8223e0ffaa76467b95cd92e611a66894
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-11-07 13:14:53 +0900

    オーバービューをヘッダとアウトレットの構成に変更。appコンポーネントのhtmlが参照する

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ViewModule } from './view/view.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
