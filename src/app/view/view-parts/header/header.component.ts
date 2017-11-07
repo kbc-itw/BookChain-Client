26cd47eb1c2914b293714e397dbd54efb4629d73
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-11-07 12:45:48 +0900

    ヘッダ用コンポーネントの作成

---
+++ src/app/view/view-parts/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bookchain-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass'],
+  // encapsulation: ViewEncapsulation.None
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/view/view-parts/view-parts.module.ts
+++ src/app/view/view-parts/view-parts.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material';
+
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
   ],
-  declarations: [HeaderComponent]
+  declarations: [HeaderComponent],
+  exports: [ HeaderComponent ]
 })
 export class ViewPartsModule { }
