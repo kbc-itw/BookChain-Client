1c44d6bd4d135f92e0f0ad11e265c1144f04220c
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-11-08 10:06:16 +0900

    appモジュールでimportするモジュールをBrowserModuleからAngular MaterialのBrowserAnimationsModuleに変更

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ViewModule } from './view/view.module';
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ViewModule
   ],
