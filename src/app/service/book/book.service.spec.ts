43c00acbe65247c574e54af862ab4722393367ab
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-11-09 15:55:18 +0900

    テスト記述

--- src/app/service/book/book.service.spec.ts
+++ src/app/service/book/book.service.spec.ts
@@ -1,8 +1,19 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { Book } from '../../model/book/book';
 import { BookService } from './book.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
+import { asIsbn10 } from 'isbn-utils';
+
+const expectedResult: Book =  {
+  id:            'm2BpPQAACAAJ',
+  isbn10:        '4062639149',
+  isbn13:        '9784062639149',
+  title:         '天空の蜂',
+  authors:       ['東野圭吾'],
+  publishedDate: '1998',
+  thumbnailURL:  'http://books.google.com/books/content?id=m2BpPQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+};
 
 describe('BookService', () => {
   beforeEach(() => {
@@ -28,5 +39,21 @@ describe('BookService', () => {
       .catch(err => expect(err).toThrowError('Invalid ISBN'));
   }));
 
+  it('ISBN10桁(天空の蜂)', inject([BookService], (service: BookService) => {
+    service.get(expectedResult.isbn10)
+      .then( (book: Book) => expect(book).toEqual(expectedResult));
+  }));
 
+
+  it('ISBN13桁(天空の蜂）', inject([BookService], (service: BookService) => {
+    service.get(expectedResult.isbn13)
+      .then( (book: Book) => expect(book).toEqual(expectedResult));
+  }));
+
+  it('キャッシュから取得(天空の蜂)', inject([BookService], (service: BookService) => {
+    service.get(expectedResult.isbn13)
+      .then( (book: Book) => service.get(expectedResult.isbn13))
+      .then( (book: Book) => service.get('4043636032'))
+      .then( (cached: Book) => expect(cached).toEqual(expectedResult));
+  }));
 });
