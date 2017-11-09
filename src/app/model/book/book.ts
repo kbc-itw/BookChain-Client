1f5f615e4a161dbdb70a23dfb656c24f7414b9e2
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-11-09 10:17:01 +0900

    書籍用インタフェースの作成

---
+++ src/app/model/book/book.ts
@@ -0,0 +1,20 @@
+/**
+ * 書籍扱いのインタフェース。
+ * 現在、手入力シーケンスは存在せず、Google Books APIsから取得したデータをもとに生成することになっている。
+ * よって、生成後の変更は認めないものとし、安全のためreadonlyを指定する。
+ * @author kbc14a12
+ */
+export interface Book {
+
+    // このシステムで割り振るidでなく、Google Books APIsが用意するid。
+    // これを利用すると、Google Books上の該当書籍のページへのリンクが生成できる。
+    readonly id: string;
+
+    // isbn10は13に再計算する。
+    readonly isbn13: string;
+
+    readonly title: string;
+    readonly authors: string[];
+    readonly publishedDate: string;
+    readonly thumbnailURL: string;
+}
