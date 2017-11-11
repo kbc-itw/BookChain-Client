20562a1b047d7d59573218493f662d406093a8b8
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-11-11 22:22:15 +0900

    テストが命令を網羅するようテストケースを追加

--- src/app/service/book/book-cache.service.spec.ts
+++ src/app/service/book/book-cache.service.spec.ts
@@ -3,7 +3,33 @@ import { TestBed, inject } from '@angular/core/testing';
 import { BookCacheService } from './book-cache.service';
 import { IBook } from '../../model/book/ibook';
 
-describe('BookCacheService', () => {
+describe('ストレージがそもそもない', () => {
+
+  let fakeStorage;
+  let getItemSpy;
+  let setItemSpy;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [BookCacheService]
+    });
+    fakeStorage = {};
+      // ローカルストレージの置き換え
+      getItemSpy = spyOn(localStorage, 'getItem').and.callFake((key) =>  {
+        return JSON.stringify(fakeStorage[key]);
+      });
+      setItemSpy = spyOn(localStorage, 'setItem').and.callFake((key, value) =>  {
+        fakeStorage[key] = JSON.parse(value);
+      });
+  });
+
+  it('インスタンス生成', inject([BookCacheService], (service: BookCacheService) => {
+    expect(service).toBeTruthy();
+  }));
+
+});
+
+describe('ストレージ内にデータがない', () => {
 
   let testData: IBook;
   let fakeStorage;
@@ -15,6 +41,10 @@ describe('BookCacheService', () => {
     TestBed.configureTestingModule({
       providers: [BookCacheService]
     });
+
+    it('インスタンス生成', inject([BookCacheService], (service: BookCacheService) => {
+      expect(service).toBeTruthy();
+    }));
 
     // ダミー本とダミーローカルストレージの用意
     testData =  {
@@ -39,10 +69,6 @@ describe('BookCacheService', () => {
 
   });
 
-  it('インスタンス生成', inject([BookCacheService], (service: BookCacheService) => {
-    expect(service).toBeTruthy();
-  }));
-
   it('add 成功する場合', inject([BookCacheService], (service: BookCacheService) => {
     expect(service.add(testData)).toBeTruthy();
   }));
@@ -62,9 +88,38 @@ describe('BookCacheService', () => {
   it('getAll', inject([BookCacheService], (service: BookCacheService) => {
     expect(service.getAll()).toEqual([]);
   }));
+});
 
-  describe('ストレージあり', () => {
+
+
+describe('ストレージあり', () => {
+
+    let testData: IBook;
+    let fakeStorage;
+
+    let getItemSpy;
+    let setItemSpy;
+
     beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [BookCacheService]
+      });
+
+      it('インスタンス生成', inject([BookCacheService], (service: BookCacheService) => {
+        expect(service).toBeTruthy();
+      }));
+
+      // ダミー本とダミーローカルストレージの用意
+      testData =  {
+        id:            'm2BpPQAACAAJ',
+        isbn10:        '4062639149',
+        isbn13:        '9784062639149',
+        title:         '天空の蜂',
+        authors:       ['東野圭吾'],
+        publishedDate: '1998',
+        thumbnailURL:  'http://books.google.com/books/content?id=m2BpPQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+      };
+
       fakeStorage = {
         'bookchain-Angular-book': {
           '9784062639149': {
@@ -82,48 +137,55 @@ describe('BookCacheService', () => {
             isbn13: '9784309024226',
             title: 'カラマーゾフの兄弟',
             authors: ['亀山郁夫'],
-            publicshedData: '2015-11-22',
+            publishedDate: '2015-11-22',
             thumbnailURL: 'http://books.google.com/books/content?id=GEX6jgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
           }
         }
       };
-    });
 
-    it('add 失敗する場合', inject([BookCacheService], (service: BookCacheService) => {
-      expect(service.add(testData)).toBeFalsy();
-    }));
+      // ローカルストレージの置き換え
+      getItemSpy = spyOn(localStorage, 'getItem').and.callFake((key) =>  {
+        return JSON.stringify(fakeStorage[key]);
+      });
+      setItemSpy = spyOn(localStorage, 'setItem').and.callFake((key, value) =>  {
+        fakeStorage[key] = JSON.parse(value);
+      });
 
-    it('remove 成功する場合', inject([BookCacheService], (service: BookCacheService) => {
-      expect(service.remove(testData)).toBeTruthy(true);
-    }));
-
-    it('get 見つかる場合', inject([BookCacheService], (service: BookCacheService) => {
-      expect(service.get(testData.isbn13)).toEqual(testData);
-    }));
-
-    it('update 成功する場合', inject([BookCacheService], (service: BookCacheService) => {
-      expect(service.update(testData)).toBeTruthy();
-    }));
-
-    it('clear', inject([BookCacheService], (service: BookCacheService) => {
-      service.clear();
-      expect(fakeStorage['bookchain-Angular-book']).toEqual({});
-    }));
-
-    it('getAll', inject([BookCacheService], (service: BookCacheService) => {
-      const kameyama = {
-        id: 'GEX6jgEACAAJ',
-        isbn10: '430902422X',
-        isbn13: '9784309024226',
-        title: 'カラマーゾフの兄弟',
-        authors: ['亀山郁夫'],
-        publishedData: '2015-11-22',
-        thumbnailURL: 'http://books.google.com/books/content?id=GEX6jgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
-      };
-      const resultArray = service.getAll();
-      expect(resultArray[0]).toEqual(testData);
-      expect(resultArray[1]).toEqual(kameyama);
-    }));
   });
 
+  it('add 失敗する場合', inject([BookCacheService], (service: BookCacheService) => {
+    expect(service.add(testData)).toBeFalsy();
+  }));
+
+  it('remove 成功する場合', inject([BookCacheService], (service: BookCacheService) => {
+    expect(service.remove(testData)).toBeTruthy(true);
+  }));
+
+  it('get 見つかる場合', inject([BookCacheService], (service: BookCacheService) => {
+    expect(service.get(testData.isbn13)).toEqual(testData);
+  }));
+
+  it('update 成功する場合', inject([BookCacheService], (service: BookCacheService) => {
+    expect(service.update(testData)).toBeTruthy();
+  }));
+
+  it('clear', inject([BookCacheService], (service: BookCacheService) => {
+    service.clear();
+    expect(fakeStorage['bookchain-Angular-book']).toEqual({});
+  }));
+
+  it('getAll', inject([BookCacheService], (service: BookCacheService) => {
+    const kameyama = {
+      id: 'GEX6jgEACAAJ',
+      isbn10: '430902422X',
+      isbn13: '9784309024226',
+      title: 'カラマーゾフの兄弟',
+      authors: ['亀山郁夫'],
+      publishedDate: '2015-11-22',
+      thumbnailURL: 'http://books.google.com/books/content?id=GEX6jgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+    };
+    const resultArray = service.getAll();
+    expect(resultArray[0]).toEqual(testData);
+    expect(resultArray[1]).toEqual(kameyama);
+  }));
 });
