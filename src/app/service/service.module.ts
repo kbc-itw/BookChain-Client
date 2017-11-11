ad59e42b6c25f70faa1cacbefdae6fd7f9d0786d
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-11-11 22:12:42 +0900

    サービスモジュールに書籍キャッシュ用サービスを追加

--- src/app/service/service.module.ts
+++ src/app/service/service.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookService } from './book/book.service';
 import { GoogleBooksApisGatewayService } from './book/google-books-apis-gateway.service';
+import { BookCacheService } from './book/book-cache.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [],
-  providers: [BookService, GoogleBooksApisGatewayService]
+  providers: [BookService, GoogleBooksApisGatewayService, BookCacheService]
 })
 export class ServiceModule { }
