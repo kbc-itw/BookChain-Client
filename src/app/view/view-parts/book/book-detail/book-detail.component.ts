015ada3cab3b160fcb79de6a12962ebe88de1f93
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-11-16 11:02:16 +0900

    書籍詳細コンポーネントの実装

--- src/app/view/view-parts/book/book-detail/book-detail.component.ts
+++ src/app/view/view-parts/book/book-detail/book-detail.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IBook } from '../../../../model/book/ibook';
 
 @Component({
   selector: 'bookchain-book-detail',
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.sass'],
-  //encapsulation: ViewEncapsulation.None
+  // encapsulation: ViewEncapsulation.None
 })
 export class BookDetailComponent implements OnInit {
+
+  @Input() book: IBook;
+  private authors: String;
 
   constructor() { }
 
   ngOnInit() {
+    this.authors = this.book.authors.join(', ');
   }
 
 }
