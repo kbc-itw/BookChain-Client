c7b91959c8aae14ad5552b3a4cf7023ac3114a42
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-11-16 09:53:57 +0900

    コメント

--- src/app/service/book/book.service.ts
+++ src/app/service/book/book.service.ts
@@ -5,26 +5,42 @@ import { IBook } from '../../model/book/ibook';
 import * as ISBN from 'isbn-utils';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/throw';
 
+/**
+ * 書籍情報を取得するためのサービス。
+ * @author kbc14a12
+ */
 @Injectable()
 export class BookService {
 
   constructor(private api: GoogleBooksApisGatewayService, private cache: BookCacheService) { }
 
+  /**
+   * ISBNコードによって書籍情報を取得する
+   * @param isbn ISBNコード
+   * @returns 取得した書籍データを流すObservable。取得失敗時にエラーが流れる。
+   */
   public getByISBN(isbn: string): Observable<IBook> {
 
     if (!ISBN.isValid(isbn)) {
       return Observable.throw(new Error('Invalid ISBN'));
     }
+
     const isbn13: string = ISBN.asIsbn13(isbn);
 
+    // キャッシュにあればキャッシュを利用する。なければapiを叩く。
     const cachedBook = this.cache.get(isbn13);
     if (cachedBook) {
       Observable.of(cachedBook);
     }
-    return this.api.get({isbn: isbn13}).first();
+
+    this.api.get({isbn: isbn13})
+      .first()
+      // mapをこのように利用するべきではないと思う
+      .map(book => {this.cache.add(book); return book; });
   }
 
 }
