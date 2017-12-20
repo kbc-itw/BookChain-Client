425f32b6786623272bba00e3ffea8df5e6f26566
Ryunosuke Kubo <kbc16a07@stu.kawahara.ac.jp>
2017-12-21 00:39:17 +0900

    プロフィールページルーティング追加

--- src/app/view/view-routing.module.ts
+++ src/app/view/view-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
   }
 ];
 
