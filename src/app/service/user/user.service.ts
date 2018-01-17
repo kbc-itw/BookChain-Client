9eb8d670b4889f16580652d611ee8a68ff3b019a
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2018-01-17 14:39:34 +0900

    userに対するgetの実装

--- src/app/service/user/user.service.ts
+++ src/app/service/user/user.service.ts
@@ -3,10 +3,12 @@ import { RestApiGateway } from '../rest-api-gateway';
 import { IUser, isLocator, isID } from '../../model/user/iuser';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/Observable/of';
-
+import { HttpClient } from '@angular/common/http';
+import * as queryString from 'query-string';
+import { API_BASE_URL } from '../../../environments/environment';
 export class UserService implements RestApiGateway<IUser> {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   /**
    * HTTPのgetメソッド
@@ -14,20 +16,15 @@ export class UserService implements RestApiGateway<IUser> {
    * @returns 取得した情報が流れるObservable
    */
   get(params: {[key: string]: string}): Observable<IUser> {
-    const id = 'huruikagi';
-    const locator = 'huruikagi@localhost';
-    if (isLocator(locator) && isID(id)) {
-      return Observable.of({
-        id: id,
-        host: 'localhost',
-        locator: locator,
-        name: 'ふるいかぎ'
-      });
-  } else {
-    return Observable.throw(new Error('は？'));
-  }
+    const queries = queryString.stringify(params);
+    const targetURL = API_BASE_URL + 'user' + ( queries === '' ? '' : '?' + queries);
+    return this.http.get<IUser>(targetURL);
 }
 
+
+  /**
+   * TODO
+   */
   getLoginUser(): Observable<IUser> {
     const id = 'tomo_space';
     const locator = id + '@' + 'localhost';
