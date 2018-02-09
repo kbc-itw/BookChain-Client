866f255d5bf9819483b7890b838e73a74a842d2b
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2018-02-09 16:59:46 +0900

    なんでワシの環境でエラー吐かなかったんですかね？

--- src/app/service/user/user.service.ts
+++ src/app/service/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { RestApiGateway } from '../rest-api-gateway';
 import { IUser, isLocator, isID } from '../../model/user/iuser';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/Observable/of';
+import 'rxjs/add/observable/of';
 import { HttpClient } from '@angular/common/http';
 import * as queryString from 'query-string';
 import { API_BASE_URL } from '../../../environments/environment';
