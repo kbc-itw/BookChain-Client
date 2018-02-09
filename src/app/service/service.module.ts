e5f12cb04a2a98372f2837869a043f7dddcb78e2
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2018-02-09 12:11:34 +0900

    master側のファイルを利用するため削除

--- src/app/service/service.module.ts
+++ src/app/service/service.module.ts
@@ -6,9 +6,7 @@ import { GoogleBooksApisGatewayService } from './book/google-books-apis-gateway.
 import { BookCacheService } from './book/book-cache.service';
 import { RestApiGateway } from './rest-api-gateway';
 import { BookGateway } from './book/book-gateway';
-import { UserService } from './user.service';
-import { OwnershipGatewayService } from './ownership-gateway.service';
-import { OwnershipService } from './ownership.service';
+import { UserService } from './user/user.service';
 import { TradingService } from './trading/trading.service';
 import { RoomService } from './room/room.service';
 import { UserRegisterService } from './user/user-register.service';
@@ -19,6 +17,6 @@ import { UserRegisterService } from './user/user-register.service';
   ],
   declarations: [],
   providers: [BookService, GoogleBooksApisGatewayService, BookCacheService, UserService,
-  TradingService, OwnershipGatewayService, OwnershipService, RoomService, UserRegisterService, UserRegisterService]
+  TradingService, OwnershipService, RoomService, UserRegisterService, UserRegisterService]
 })
 export class ServiceModule { }
