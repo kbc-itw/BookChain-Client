524fdea1503ae2d9480d823d22cdb5184db24da4
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2018-02-15 13:52:16 +0900

    decodeしてなかった問題

--- src/app/view/trade-guest/trade-guest.component.ts
+++ src/app/view/trade-guest/trade-guest.component.ts
@@ -76,6 +76,7 @@ export class TradeGuestComponent implements OnInit {
           }
         };
       };
+      qrcodeReader.decode(this.qrcodeRawImageReader.result);
     };
 
     this.barcodeRawImageReader.onload = (progressEvent) => {
